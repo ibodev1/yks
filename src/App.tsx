51835e8ee7fd2fa0b474029707d8c333e2ddd16c
Ibrahim Odev <ibrahimodev42@gmail.com>
2023-03-18 15:30:30 +0300

    @vercel/analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import type { Component } from "solid-js";
+import { inject } from "@vercel/analytics";
 import CountDown from "./components/CountDown";
 import DateClock from "./components/DateClock";
 import Quote from "./components/Quote";
 import Working from "./components/Working";
+
+inject();
 
 const App: Component = () => {
   return (
